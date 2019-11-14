dfbb4753bcf3224c21f2bea41cbc377546956e9b
nataphilips <fnd.natalia@gmail.com>
2019-11-14 12:39:57 +0200

    Extracted Pokehouse component, added a drop down for pokemon choosing

--- src/App.js
+++ src/App.js
@@ -2,51 +2,21 @@ import React from "react";
 import { getPokemon } from "./utils";
 import pokeball from "./pokeball.png";
 import "./App.css";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPoo } from "@fortawesome/free-solid-svg-icons";
+import PokeHouse from "./PokeHouse";
 
-function App({ name }) {
+function App({ name, playCounter, setPlayCounter, setFoodCounter, setPooped }) {
   const [data, setData] = React.useState(() =>
     JSON.parse(localStorage.getItem("data"))
   );
   const [pokename, setPokeName] = React.useState(() =>
     localStorage.getItem("pokename")
   );
-  const [playCounter, setPlayCounter] = React.useState(
-    JSON.parse(localStorage.getItem("counter")) || 0
-  );
-  const [foodCounter, setFoodCounter] = React.useState(
-    JSON.parse(localStorage.getItem("foodcounter")) || 4
-  );
-  const [pooped, setPooped] = React.useState(false);
-  const [hungry, setHungry] = React.useState(false);
-  const [cleaning, setCleaning] = React.useState(false);
-  const [msg, setMsg] = React.useState("");
-  const funmeter = ["😴", "💛", "💛💛", "💛💛💛", "💛💛💛💛"];
-  const food = ["HUNGRY", "🍎", "🍎🍎", "🍎🍎🍎", "🍎🍎🍎🍎"];
 
-  // const [lives, setLives] = React.useState(["♥", "♥♥", "♥♥♥", "♥♥♥♥", "♥♥♥♥♥"]);
-
-  // console.log(lives);
   React.useEffect(() => {
     localStorage.setItem("pokename", pokename);
     localStorage.setItem("data", JSON.stringify(data));
-    localStorage.setItem("counter", playCounter);
-    localStorage.setItem("foodcounter", foodCounter);
-  }, [data, playCounter, pokename, foodCounter]);
+  }, [data, pokename]);
 
-  const handlePlay = () => {
-    setPlayCounter(playCounter + 1);
-    setFoodCounter(foodCounter - 1);
-    if (playCounter > 3) {
-      setPlayCounter(0);
-      setPooped(true);
-      setMsg("Whoops!");
-    } else if (foodCounter === 1) {
-      setHungry(true);
-      setMsg("Feed me, please!");
-    }
-  };
   const handleSearch = event => {
     event.preventDefault();
     getPokemon(pokename).then(data => {
@@ -55,30 +25,6 @@ function App({ name }) {
       setPlayCounter(0);
       setPooped(false);
     });
-  };
-  const handlePoops = () => {
-    setPooped(false);
-    setMsg("Thanks!");
-    setCleaning(true);
-    setTimeout(() => {
-      setCleaning(false);
-    }, 1000);
-    setTimeout(() => {
-      setMsg("");
-    }, 2000);
-    if (foodCounter === 0) {
-      console.log("I'm hungry too!");
-      setHungry(true);
-      console.log(hungry);
-    }
-  };
-  const handleFood = () => {
-    setHungry(false);
-    setFoodCounter(4);
-    setMsg("Tasty!");
-    setTimeout(() => {
-      setMsg("");
-    }, 2000);
   };
 
   return (
@@ -89,67 +35,40 @@ function App({ name }) {
       <form>
         <label htmlFor="pokeSearch">Pick a pokemon</label>
         <input
+          name="pokemons"
           type="text"
           id="pokeSearch"
           value={pokename}
           onChange={event => setPokeName(event.target.value)}
         />
         <button id="callPoke" onClick={handleSearch} data-testid="search">
-          <img id="callpoke" src={pokeball} />
+          <img id="callpoke" alt="pokeball" src={pokeball} />
         </button>
       </form>
-      {data && data.id && (
-        <div id="gameContainer">
-          <h2 id="pokeName" data-testid="pokename">
-            {data.name}
-          </h2>
-          <div className="pokeHouse">
-            {cleaning && <div id="bubbles"></div>}
-            <div id="message">
-              <div id="speech">{msg ? msg : "Play with me!"}</div>
-            </div>
-            <div id="pokemonZone">
-              <img
-                id="pokemon"
-                src={
-                  !pooped
-                    ? data.sprites.front_default
-                    : data.sprites.back_default
-                }
-                alt={`${data.name} default sprite`}
-              />
-
-              <div
-                id="poopContainer"
-                style={{
-                  transform: pooped && "translateY(30%)",
-                  transition: pooped && "all 1s ease-in",
-                  visibility: !pooped ? "hidden" : "visible"
-                }}
-              >
-                <FontAwesomeIcon icon={faPoo} id="poop" />
-              </div>
-            </div>
-          </div>
-          <div class="meters">
-            <span class="stats">Fun: {funmeter[playCounter]}</span>
-          </div>
-          <div class="meters">
-            <span class="stats">Food :{food[foodCounter]}</span>
-          </div>
-          <div id="btnCont">
-            <button onClick={handlePlay} disabled={pooped || hungry}>
-              Play
-            </button>
-            <button onClick={handleFood} disabled={!hungry}>
-              Feed
-            </button>
-            <button onClick={handlePoops} disabled={!pooped}>
-              Clean
-            </button>
-          </div>
-        </div>
-      )}
+      <form>
+        <label htmlFor="pokeSearch">Or choose one of those</label>
+        <select
+          name="pokemons"
+          type="text"
+          id="pokeSearch"
+          value={pokename}
+          onChange={event => setPokeName(event.target.value)}
+        >
+          <option value="pikachu">pikachu</option>
+          <option value="mewtwo">mewtwo</option>
+          <option value="persian">persian</option>
+          <option value="squirtle">squirtle</option>
+          <option value="meowth">meowth</option>
+          <option value="charizard">charizard</option>
+          <option value="eevee">eevee</option>
+          <option value="psyduck">psyduck</option>
+          <option value="slowpoke">slowpoke</option>
+        </select>
+        <button id="callPoke" onClick={handleSearch} data-testid="search">
+          <img id="callpoke" alt="pokeball" src={pokeball} />
+        </button>
+      </form>
+      {data && data.id && <PokeHouse data={data} pokename={pokename} />}
     </div>
   );
 }
